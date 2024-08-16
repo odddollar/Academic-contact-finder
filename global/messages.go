85a70da9cc2207d019c9c116ae8a8316fca9b09d
Simon Eason <sie.eason@gmail.com>
2024-08-16 20:01:01 +0800

    Added standard error dialog function

--- global/messages.go
+++ global/messages.go
@@ -1,9 +1,15 @@
 package global
 
-import "fyne.io/fyne/v2/dialog"
+import (
+	"fyne.io/fyne/v2/dialog"
+)
 
 // Standard dialog to show success
 func ShowSuccess(message string) {
-	// Show confirmation dialog
 	dialog.ShowInformation("Success", message, W)
+}
+
+// Standard dialog to show error
+func ShowError(err error) {
+	dialog.ShowInformation("Error", err.Error(), W)
 }
