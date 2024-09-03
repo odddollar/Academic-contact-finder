a71be8e876aac5599c4bf51353080b77de345700
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-09-03 23:44:29 +0800

    Email test code should be multi platform, unable to test

--- email/email.go
+++ email/email.go
@@ -3,6 +3,7 @@ package email
 import (
 	"fmt"
 	"os/exec"
+	"runtime"
 
 	"github.com/odddollar/CITS3200-Project/global"
 )
@@ -21,7 +22,17 @@ func SendEmailTest() { // (address string, subject string, body string) {
 	subject := "TestEmail"
 	body := "Test Email Body"
 	mailToURL := fmt.Sprintf("mailto:%s?subject=%s&body=%s", address, subject, body)
-	exec.Command("powershell", "-Command", fmt.Sprintf("Start-Process '%s'", mailToURL)).Run()
+	switch runtime.GOOS {
+	case "windows":
+		// Use PowerShell to open the mailto URL on Windows
+		exec.Command("powershell", "-Command", fmt.Sprintf("Start-Process '%s'", mailToURL)).Run()
+	case "linux":
+		// Use xdg-open to open the mailto URL on Linux
+		exec.Command("xdg-open", mailToURL).Run()
+	case "darwin":
+		// Use open to open the mailto URL on macOS
+		exec.Command("open", mailToURL).Run()
+	}
 }
 
 // Send email to address of all returned results
