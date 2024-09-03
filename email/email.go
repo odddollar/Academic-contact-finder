d9d6f42b08e49b32cb6d8f7e60194e04bc40fa93
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-09-03 22:27:16 +0800

    added popup before and after email test

--- email/email.go
+++ email/email.go
@@ -17,11 +17,13 @@ func SendEmail(address string, details []global.FoundContactStruct) {
 }
 
 func SendEmailTest() { // (address string, subject string, body string) {
+	global.ShowSuccess("Start of Email Test")
 	address := "example@example.com"
 	subject := "TestEmail"
 	body := "TestEmailBody"
 	mailToURL := fmt.Sprintf("mailto:%s?subject=%s&body=%s", address, subject, body)
-	exec.Command("Cmd", "/c", "start", mailToURL).Run()
+	exec.Command("Cmd", "/c", "start", mailToURL).Start()
+	global.ShowSuccess("End of Email Test")
 }
 
 // Send email to address of all returned results
