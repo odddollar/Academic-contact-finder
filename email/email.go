a454188eadd5c9a687401aff5deb2c98625c6f68
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-09-04 01:22:51 +0800

    Test code removed, emailing should now be implemented fully

--- email/email.go
+++ email/email.go
@@ -42,41 +42,6 @@ func SendEmail(address string, details []global.FoundContactStruct) {
 	global.ShowSuccess("Email sent to: " + address)
 }
 
-func SendEmailTest() {
-	address := "example@example.com"
-	subject := "TestEmail"
-
-	dummyStruct := global.FoundContactStruct{
-		Name:        "Test Name",
-		Salutation:  "Test Salutation",
-		Email:       "Test@email.com",
-		Institution: "Test Institution",
-		URL:         &url.URL{Scheme: "https", Host: "www.example.com"},
-	}
-	bodyContent := fmt.Sprintf(
-		"Name: %s\nSalutation: %s\nEmail: %s\nInstitution: %s\nURL: %s\n",
-		dummyStruct.Name,
-		dummyStruct.Salutation,
-		dummyStruct.Email,
-		dummyStruct.Institution,
-		dummyStruct.URL.String(),
-	)
-	encodedBodyContent := url.QueryEscape(bodyContent)
-	encodedBodyContent = strings.ReplaceAll(encodedBodyContent, "+", "%20")
-	mailToURL := fmt.Sprintf("mailto:%s?subject=%s&body=%s", address, subject, encodedBodyContent)
-	switch runtime.GOOS {
-	case "windows":
-		// Use PowerShell to open the mailto URL on Windows
-		exec.Command("powershell", "-Command", fmt.Sprintf("Start-Process '%s'", mailToURL)).Run()
-	case "linux":
-		// Use xdg-open to open the mailto URL on Linux
-		exec.Command("xdg-open", mailToURL).Run()
-	case "darwin":
-		// Use open to open the mailto URL on macOS
-		exec.Command("open", mailToURL).Run()
-	}
-}
-
 // Send email to address of all returned results
 func EmailAll() {
 	SendEmail(global.A.Preferences().String("Default_email"), global.AllFoundContacts)
