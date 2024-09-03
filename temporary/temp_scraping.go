be352fc4b5b34554a8c0a5d20f6386c4774f8a5d
Simon Eason <sie.eason@gmail.com>
2024-09-03 12:37:50 +0800

    Iterating through urls and checking results for matches

--- temporary/temp_scraping.go
+++ temporary/temp_scraping.go
@@ -10,15 +10,22 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
+
+type result struct {
+	first string
+	last  string
+	email string
+}
 
 func main() {
 	// Create a new Chrome browser context with options to disable headless mode
 	opts := append(
 		chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),       // Disable headless mode
-		chromedp.Flag("disable-gpu", false),    // Enable GPU usage
-		chromedp.Flag("start-maximized", true), // Start maximized
+		chromedp.Flag("headless", false),        // Disable headless mode
+		chromedp.Flag("disable-gpu", false),     // Enable GPU usage
+		chromedp.Flag("start-maximized", false), // Start maximized
 	)
 
 	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
@@ -29,8 +36,35 @@ func main() {
 	defer cancel()
 
 	// URL you want to visit
-	url := "https://www.scopus.com/record/display.uri?eid=2-s2.0-85079320615&origin=resultslist&sort=plf-f&src=s&st1=McDonald&st2=Chris&nlo=1&nlr=20&nls=count-f&sid=2a239e24d72b73730a45aca45c5e6c37&sot=anl&sdt=aut&sl=39&s=AU-ID%28%22McDonald%2c+Chris+S.%22+57169566400%29&relpos=2&citeCnt=133&searchTerm="
+	url := []string{
+		"https://www.scopus.com/record/display.uri?eid=2-s2.0-85079320615&origin=resultslist",
+		"https://www.scopus.com/record/display.uri?eid=2-s2.0-85070927816&origin=resultslist",
+		"https://www.scopus.com/record/display.uri?eid=2-s2.0-85037348791&origin=resultslist",
+		"https://www.scopus.com/record/display.uri?eid=2-s2.0-85119400640&origin=resultslist",
+		"https://www.scopus.com/record/display.uri?eid=2-s2.0-84922537253&origin=resultslist",
+	}
 
+	firstName := "chris"
+	lastName := "mcdonald"
+
+	// Iterate through urls
+	for _, i := range url {
+		fmt.Println("Source: ", i)
+		results := findEmails(i, ctx)
+
+		for _, j := range results {
+			if fuzzy.MatchFold(j.first, firstName) && fuzzy.MatchFold(j.last, lastName) {
+				fmt.Printf("Match %s\n", j)
+			} else {
+				fmt.Printf("No match %s\n", j)
+			}
+		}
+
+		fmt.Println()
+	}
+}
+
+func findEmails(url string, ctx context.Context) []result {
 	// Create a variable to store the page's HTML
 	var htmlContent string
 
@@ -50,6 +84,8 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var toReturn []result
+
 	// Find all <li> elements
 	doc.Find("li").Each(func(i int, s *goquery.Selection) {
 		// Find all <a> elements with href attributes
@@ -58,8 +94,14 @@ func main() {
 			if exists && strings.HasPrefix(href, "mailto:") {
 				// Find the <span> within the <button> to get the name
 				name := s.Find("button span").Text()
-				fmt.Printf("%s: %s\n", name, href[7:])
+				toReturn = append(toReturn, result{
+					strings.Split(name, ", ")[1],
+					strings.Split(name, ", ")[0],
+					href[7:],
+				})
 			}
 		})
 	})
+
+	return toReturn
 }
