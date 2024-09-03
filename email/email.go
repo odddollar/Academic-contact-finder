c0e9c3dc12a2d56adf448a036a90f32156e521ae
UpstandingVlad <164867745+UpstandingVlad@users.noreply.github.com>
2024-09-04 01:02:14 +0800

    Altered email test code to use a struct of the same format as the FoundContactStruct

--- email/email.go
+++ email/email.go
@@ -2,6 +2,7 @@ package email
 
 import (
 	"fmt"
+	"net/url"
 	"os/exec"
 	"runtime"
 
@@ -20,8 +21,24 @@ func SendEmail(address string, details []global.FoundContactStruct) {
 func SendEmailTest() { // (address string, subject string, body string) {
 	address := "example@example.com"
 	subject := "TestEmail"
-	body := "Test Email Body"
-	mailToURL := fmt.Sprintf("mailto:%s?subject=%s&body=%s", address, subject, body)
+	//body := "Test Email Body"
+
+	dummyStruct := global.FoundContactStruct{
+		Name:        "Test Name",
+		Salutation:  "Test Salutation",
+		Email:       "Test@email.com",
+		Institution: "Test Institution",
+		URL:         &url.URL{Scheme: "https", Host: "www.example.com"},
+	}
+	bodyContent := fmt.Sprintf(
+		"Name: %s\nSalutation: %s\nEmail: %s\nInstitution: %s\nURL: %s",
+		dummyStruct.Name,
+		dummyStruct.Salutation,
+		dummyStruct.Email,
+		dummyStruct.Institution,
+		dummyStruct.URL.String(),
+	)
+	mailToURL := fmt.Sprintf("mailto:%s?subject=%s&body=%s", address, subject, bodyContent)
 	switch runtime.GOOS {
 	case "windows":
 		// Use PowerShell to open the mailto URL on Windows
