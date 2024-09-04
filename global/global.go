e04452ff37880389bb9c95cdb3dd2f8c907b9058
Simon Eason <sie.eason@gmail.com>
2024-09-04 15:18:18 +0800

    Added second newline to stringer

--- global/global.go
+++ global/global.go
@@ -48,5 +48,5 @@ type FoundContactStruct struct {
 
 // Implement Stringer interface for FoundContactStruct
 func (fcs FoundContactStruct) String() string {
-	return fmt.Sprintf("%s %s, %s\n%s\n%s\nSource: %s\n", fcs.FirstName, fcs.LastName, fcs.Salutation, fcs.Email, fcs.Institution, fcs.URL.String())
+	return fmt.Sprintf("%s %s, %s\n%s\n%s\nSource: %s\n\n", fcs.FirstName, fcs.LastName, fcs.Salutation, fcs.Email, fcs.Institution, fcs.URL.String())
 }
